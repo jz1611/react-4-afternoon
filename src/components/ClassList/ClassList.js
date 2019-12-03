72aef1b5495acb258372abd5d561d380dc0b0488
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-12-03 16:29:16 -0700

    Completed Black Diamond

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -26,9 +26,10 @@ export default class ClassList extends Component {
         <h3>{student.first_name} {student.last_name}</h3>
       </Link>
     ))
-
+    
     return (
       <div className="box">
+        <Link to="/"><button>Back</button></Link>
         <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
         {students}
